0d3190f089920800b9bc807dab7c8a0cec31da1b
utkarsh pandey <utkatshpandey123@gmail.com>
2026-06-17 10:22:15 +0530

    added functions

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,3 +1,26 @@
+def create_note ():
+    title=input("input title: ")
+    note_list[title]=input("write your note: ")
+
+def show_note():
+     print (note_list.keys())
+     ask=input("do you want to see your note Y/N: ").capitalize
+     if ask=="Y":
+
+            lists=input("write which title you want to see: ")
+            print(note_list.get(lists))
+     else:
+          pass
+
+def delete_note():
+     print(note_list.keys())
+     ask=input("do you want to delete your note Y/N: ").capitalize
+     if ask=="Y":
+
+            lists=input("choose which title you want to delete: ")
+            print("you have deleted the following content: ",note_list.pop(lists))
+     else:
+          pass
 
 note_list={}
 while True:
@@ -9,25 +32,12 @@ while True:
     print("choose 5 to exit the app")
     user_input=input("choose an option ")
     
-    if user_input=="1":
-
-        title=input("input title: ")
-        note=input("write your note: ")
-        storage={title:note}
-        note_list.update(storage)
-
+    if user_input=="1":      
+       create_note()
     elif user_input=="2":
-        print (note_list.keys())
-
-        lists=input("write which title you want to see: ")
-        print(note_list.get(lists))
-
-        
+        show_note()
     elif user_input=="3":
-        print(note_list.keys())
-        lists1=input("choose which title you want to delete: ")
-        print("you have deleted the following content: ",note_list.pop(lists1))
-        
+      delete_note()
     elif user_input=="4":
         pass
     elif user_input=="5":
